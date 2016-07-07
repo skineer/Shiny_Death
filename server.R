b793bf4d7059ee3422ce197d4d5cb40dd7926672
‘skineer’ <renato.pedroso.neto@gmail.com>
2016-07-07 13:08:09 -0300

    add consistency

--- server.R
+++ server.R
@@ -7,9 +7,9 @@ shinyServer(
         output$inputSmoke <- renderPrint({input$smoke})
         output$inputExercise <- renderPrint({input$exercise})
         output$prediction <- renderText({
-            if(input$age < 18 | input$age > 100) "Age must be between 18 and 100"
-            else if (input$smoke < 0 | input$smoke > 100) "Smoke must be between 0 and 100"
-            else if (input$exercise < 0 | input$exercise > 7) "Exercises must be between 0 and 7"
+            if(input$age < 18 | input$age > 100 | !is.numeric(input$age)) "Age must be between 18 and 100"
+            else if (input$smoke < 0 | input$smoke > 100 | !is.numeric(input$smoke)) "Smoke must be between 0 and 100"
+            else if (input$exercise < 0 | input$exercise > 7 | !is.numeric(input$exercise)) "Exercises must be between 0 and 7"
             else deathRisk(input$age, input$smoke, input$exercise)})
     }
 )
